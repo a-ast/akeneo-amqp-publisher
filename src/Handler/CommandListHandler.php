181fdcf8df67f3d328dc8d218c7d5887b8d0e148
Andrey Astakhov <andrey.astakhov@gmail.com>
2018-12-10 23:41:53 +0100

    Rename collection to list.

--- src/Handler/CommandListHandler.php
+++ src/Handler/CommandListHandler.php
@@ -2,12 +2,12 @@
 
 namespace Aa\Akeneo\Import\Handler;
 
-use Aa\Akeneo\ImportCommands\CommandCollectionInterface;
+use Aa\Akeneo\ImportCommands\CommandListInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class CommandListHandler implements MessageHandlerInterface
 {
-    public function __invoke(CommandCollectionInterface $commands)
+    public function __invoke(CommandListInterface $commands)
     {
         var_dump($commands);
     }
