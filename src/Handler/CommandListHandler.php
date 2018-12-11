320c4a71302b0d7679567bef8fe3d0c319dd95a5
Andrey Astakhov <andrey.astakhov@gmail.com>
2018-12-11 11:54:07 +0100

    Simple implementation of message handler.

--- src/Handler/CommandListHandler.php
+++ src/Handler/CommandListHandler.php
@@ -2,13 +2,24 @@
 
 namespace Aa\Akeneo\Import\Handler;
 
+use Aa\Akeneo\ImportCommands\CommandHandlerInterface;
 use Aa\Akeneo\ImportCommands\CommandListInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class CommandListHandler implements MessageHandlerInterface
 {
+    /**
+     * @var \Aa\Akeneo\ImportCommands\CommandHandlerInterface
+     */
+    private $handler;
+
+    public function __construct(CommandHandlerInterface $handler)
+    {
+        $this->handler = $handler;
+    }
+
     public function __invoke(CommandListInterface $commands)
     {
-        var_dump($commands);
+        $this->handler->handle($commands);
     }
 }
