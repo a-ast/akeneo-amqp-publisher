a0ca44f7c3e1cfda25b4856020789f0e54725872
Andrey Astakhov <andrey.astakhov@gmail.com>
2018-12-11 21:57:03 +0100

    Add denormalizer for command list.

---
+++ src/Serializer/CommandListNormalizer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Aa\AkeneoImport\Import\Serializer;
+
+use Aa\AkeneoImport\ImportCommands\Product\UpdateProduct;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class CommandListNormalizer implements /* NormalizerInterface, */ DenormalizerInterface
+{
+
+    use DenormalizerAwareTrait;
+
+
+//    public function normalize($object, $format = null, array $context = array())
+//    {
+//
+//    }
+//
+//    public function supportsNormalization($data, $format = null)
+//    {
+//
+//    }
+
+    public function denormalize($data, $class, $format = null, array $context = array())
+    {
+        $commands = [];
+
+        foreach ($data as $item) {
+            $commands[] = $this->denormalizer->denormalize($item, UpdateProduct::class, $format, $context);
+        }
+
+        return $commands;
+    }
+
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return 'json' === $format;
+    }
+}
