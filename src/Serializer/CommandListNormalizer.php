3a804f0f6fefb37327a87373caf00b2b5d4690d5
Andrey Astakhov <andrey.astakhov@gmail.com>
2018-12-12 17:11:31 +0100

    Implement normalization of command lists.

--- src/Serializer/CommandListNormalizer.php
+++ src/Serializer/CommandListNormalizer.php
@@ -3,22 +3,29 @@
 namespace Aa\AkeneoImport\Serializer;
 
 use Aa\AkeneoImport\ImportCommands\CommandList;
+use Aa\AkeneoImport\ImportCommands\CommandListInterface;
 use Aa\AkeneoImport\ImportCommands\Product\UpdateProduct;
+use Symfony\Component\Serializer\Exception\CircularReferenceException;
+use Symfony\Component\Serializer\Exception\InvalidArgumentException;
+use Symfony\Component\Serializer\Exception\LogicException;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
 use Symfony\Component\Serializer\SerializerInterface;
 
-class CommandListNormalizer implements DenormalizerInterface
+class CommandListNormalizer implements DenormalizerInterface, NormalizerInterface
 {
-    use DenormalizerAwareTrait;
+    use DenormalizerAwareTrait, NormalizerAwareTrait;
 
     public function denormalize($data, $class, $format = null, array $context = array())
     {
         $commands = [];
 
-        foreach ($data as $item) {
-            $commands[] = $this->denormalizer->denormalize($item, UpdateProduct::class, $format, $context);
+        // @todo: worth checking that $data['commandClass'] exist? Reflection?
+
+        foreach ($data['items'] as $item) {
+            $commands[] = $this->denormalizer->denormalize($item, $data['commandClass'], $format, $context);
         }
 
         return new CommandList($commands);
@@ -27,5 +34,24 @@ class CommandListNormalizer implements DenormalizerInterface
     public function supportsDenormalization($data, $type, $format = null)
     {
         return 'json' === $format && CommandList::class === $type;
+    }
+
+    /**
+     * @param \Aa\AkeneoImport\ImportCommands\CommandList $object
+     */
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $data = ['commandClass' => $object->getCommandClass()];
+
+        foreach ($object->getItems() as $item) {
+            $data['items'][] = $this->normalizer->normalize($item, $format, $context);
+        }
+
+        return $data;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return 'json' === $format && $data instanceof CommandList;
     }
 }
