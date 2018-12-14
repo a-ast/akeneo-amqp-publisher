1a4de82693de0c422b2344ef3eb1d9abf5db0e7b
Andrey Astakhov <andrey.astakhov@gmail.com>
2018-12-14 16:13:29 +0100

    Implement reject/requeue in the MessageHandler.

--- src/MessageHandler/CommandListHandler.php
+++ src/MessageHandler/CommandListHandler.php
@@ -2,9 +2,14 @@
 
 namespace Aa\AkeneoImport\MessageHandler;
 
+use Aa\AkeneoImport\Exception\RejectMessageException;
 use Aa\AkeneoImport\ImportCommands\CommandListHandlerInterface;
 use Aa\AkeneoImport\ImportCommands\CommandListInterface;
+use Aa\AkeneoImport\ImportCommands\Exceptions\CommandHandlerException;
+use Aa\AkeneoImport\ImportCommands\Exceptions\RecoverableCommandHandlerException;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\Transport\AmqpExt\Exception\RejectMessageExceptionInterface;
+use Throwable;
 
 class CommandListHandler implements MessageHandlerInterface
 {
@@ -18,10 +23,27 @@ class CommandListHandler implements MessageHandlerInterface
         $this->handler = $handler;
     }
 
+    /**
+     * @throws \Aa\AkeneoImport\Exception\RejectMessageException
+     * @throws \Throwable
+     */
     public function __invoke(CommandListInterface $commands)
     {
-        // @todo: add logic for requeing / rejecting messages
+        try {
+            $this->handler->handle($commands);
 
-        $this->handler->handle($commands);
+        } catch (RecoverableCommandHandlerException $e) {
+
+            throw $e;
+
+        } catch (CommandHandlerException $e) {
+
+            throw new RejectMessageException($e->getMessage(), $e->getCode(), $e);
+
+        } catch (Throwable $e) {
+
+            throw $e;
+        }
+
     }
 }
