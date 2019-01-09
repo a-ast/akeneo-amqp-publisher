b527ea03b0b464282c62e2e009f96a699c86fbda
Andrey Astakhov <andrey.astakhov@gmail.com>
2019-01-10 00:17:22 +0100

    Fix path to composer.json.

--- fix_composer_json.php
+++ fix_composer_json.php
@@ -1,9 +1,11 @@
 <?php
 
-copy(__DIR__.'/../composer.json', __DIR__.'/../composer.json.bak');
+$composerFilePath = '/composer.json';
 
-$composerJson = json_decode(file_get_contents(__DIR__.'/../composer.json'), true);
+copy(__DIR__.$composerFilePath, __DIR__.'/../composer.json.bak');
+
+$composerJson = json_decode(file_get_contents(__DIR__.$composerFilePath), true);
 
 $composerJson['config']['platform']['ext-amqp'] = '1.9.3';
 
-file_put_contents(__DIR__.'/../composer.json', json_encode($composerJson, JSON_PRETTY_PRINT));
+file_put_contents(__DIR__.$composerFilePath, json_encode($composerJson, JSON_PRETTY_PRINT));
