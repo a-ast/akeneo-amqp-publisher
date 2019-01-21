a7c3f4e933a59c38b3f1d7b9a3f6b1a2f114f67b
Andrey Astakhov <andrey.astakhov@gmail.com>
2019-01-21 12:31:52 +0100

    Implement command normalizer.

---
+++ spec/CommandHandler/Normalizer/CommandNormalizerSpec.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace spec\Aa\AkeneoImport\CommandHandler\Normalizer;
+
+use Aa\AkeneoImport\CommandHandler\Normalizer\CommandNormalizer;
+use Aa\AkeneoImport\ImportCommand\CommandInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class CommandNormalizerSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith([]);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(CommandNormalizer::class);
+        $this->shouldImplement(NormalizerInterface::class);
+        $this->shouldImplement(NormalizerAwareInterface::class);
+    }
+
+    function it_normalizes_a_command(NormalizerInterface $baseNormalizer)
+    {
+        $this->beConstructedWith(['old-code' => 'new-code']);
+
+        $baseNormalizer->normalize(Argument::any(), Argument::any(), Argument::any())->willReturn([
+            'old-code' => 1, 'color' => 'red'
+        ]);
+
+        $this->setNormalizer($baseNormalizer);
+
+        $this->normalize(Argument::any(), Argument::any(), [])->shouldBeLike([
+            'new-code' => 1, 'color' => 'red'
+        ]);
+    }
+
+    function it_supports_normalization_for_commands(CommandInterface $command)
+    {
+        $this->supportsNormalization($command, Argument::any())->shouldBe(true);
+    }
+
+    function it_does_not_support_normalization_for_non_commands()
+    {
+        $this->supportsNormalization(new class {}, Argument::any())->shouldBe(false);
+    }
+
+}
---
+++ src/CommandHandler/Normalizer/CommandNormalizer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Aa\AkeneoImport\CommandHandler\Normalizer;
+
+use Aa\AkeneoImport\ImportCommand\CommandInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class CommandNormalizer implements NormalizerInterface, NormalizerAwareInterface
+{
+    use NormalizerAwareTrait;
+
+    /**
+     * @var array
+     */
+    private $propertyNameReplaceMap;
+
+    public function __construct(array $propertyNameReplaceMap)
+    {
+        $this->propertyNameReplaceMap = $propertyNameReplaceMap;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $data = $this->normalizer->normalize($object, $format, $context);
+
+        foreach ($this->propertyNameReplaceMap as $replacingProperty => $property) {
+
+            if (!isset($data[$replacingProperty])) {
+                continue;
+            }
+
+            $data[$property] = $data[$replacingProperty];
+            unset($data[$replacingProperty]);
+
+            break;
+        }
+
+        return $data;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof CommandInterface;
+    }
+}
