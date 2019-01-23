e8ce03a28b735440de188c5294209a1dc7b614bb
Andrey Astakhov <andrey.astakhov@gmail.com>
2019-01-23 17:44:51 +0100

    Remove messenger.

--- src/Import/ImporterFactory.php
+++ src/Import/ImporterFactory.php
@@ -2,12 +2,7 @@
 
 namespace Aa\AkeneoImport\Import;
 
-use Aa\AkeneoImport\CommandBus\CommandBusFactory;
-
 class ImporterFactory
 {
-    public function create(): Importer
-    {
-        return new Importer(new CommandBusFactory());
-    }
+
 }
